1ef78fe19074408f87ff23bd8547bead7a56bdcd
Majeed <rahmanmajeed46@gmail.com>
2020-09-24 00:16:20 +0600

    [add] logger package.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const logger = require('morgan')
 
 const app = express()
+
+app.use(logger('dev'))
 
 const productRoutes = require('./routes/products-routes')
 
@@ -13,5 +16,18 @@ app.get('/', (req, res) => {
 
 app.use('/product', productRoutes)
 app.use('/orders', orderRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found')
+    error.status = 404
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.json({
+        message: err.message
+    })
+})
 
 module.exports = app
